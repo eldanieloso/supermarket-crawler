46d1b324641269c13001d789e5284881af4452e8
Daniel Ruvalcaba <danyrwits@gmail.com>
2021-11-29 23:18:40 -0600

    base crawler pupeteer

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
-import Crawler from 'crawler';
+import puppeteer from 'puppeteer';
 
-const crawler = new Crawler({
-    maxConnections: 10,
-    // This will be called for each crawled page
-    callback: (error, res, done) => {
-        if (error) console.log(error)
-        else {
-            const $ = res.$
-            console.log($('title').text())
+async function scrape(url: string) {
+    const browser = await puppeteer.launch({ headless: true })
+    const page = await browser.newPage()
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
+    await page.goto(url)
+
+    let element = await page.evaluate(() => {
+        let links = Object.values(document.body.querySelectorAll('a.name')).map((x:any) => {
+            return x.href
+        })
+        return {
+            title: document.querySelector('.product-details-name') ? document.querySelector('.product-details-name').textContent : null,
+            price: document.querySelector('.price-colour-final-pdp') ? document.querySelector('.price-colour-final-pdp').textContent : null,
+            related: links
         }
-        done();
-    }
-})
+    });
 
-crawler.queue('http://www.amazon.com')
+    await browser.close()
+
+    // logging results
+    if(element) {
+        if(element.title) console.log(element)
+        if(element.related) {
+            for (const link of element.related) {
+                await scrape(link)
+            }
+        }
+    }
+}
+
+scrape('https://www.chedraui.com.mx/Departamentos/Súper/c/MC61?q=%3Arelevance&page=0&pageSize=24')
